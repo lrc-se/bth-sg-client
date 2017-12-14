b841662e9a045352f466f32a6c972d9a361cb99f
Kalle Brisland <kabc16@student.bth.se>
2017-12-14 19:12:11 +0100

    Added event messages to chat component

--- src/components/sg-chat.vue
+++ src/components/sg-chat.vue
@@ -26,11 +26,51 @@
         
         created() {
             Client.$on("msg", this.addMessage);
+            
+            Client.$on("correct", (data) => {
+                this.addServerMessage(
+                    `* ${data.nick} gissade rätt! Det hemliga ordet var "${data.word}".`,
+                    "correct"
+                );
+            });
+            
+            Client.$on("timeout", (word) => {
+                this.addServerMessage(
+                    `* Ingen lyckades gissa rätt! Det hemliga ordet var "${word}".`,
+                    "timeout"
+                );
+            });
+            
+            Client.$on("join", (nick) => {
+                this.addServerMessage(`* ${nick} har anslutit sig till spelet.`);
+            });
+
+            Client.$on("part", (nick) => {
+                this.addServerMessage(`* ${nick} har lämnat spelet.`);
+            });
+            
+            Client.$on("drawer", (nick) => {
+                this.addServerMessage(
+                    "* Det är " + (nick.endsWith("s") ? nick : nick + "s") + " tur att rita."
+                );
+            });
+            
+            Client.$on("word", (word) => {
+                this.addServerMessage(`* Det är din tur att rita! Det hemliga ordet är "${word}".`);
+            });
         },
         
         methods: {
             addMessage(message) {
                 this.messages.push(message);
+            },
+            
+            addServerMessage(text, type) {
+                this.addMessage({
+                    type: type || "server",
+                    nick: null,
+                    text: text
+                });
             }
         }
     };
